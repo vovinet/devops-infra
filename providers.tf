87502852480b5792e5315103e83d87792bb0da20
Vladimir Zubarev <v.zubarev@inbox.ru>
2022-07-03 15:48:56 +0300

    yc_token added

--- providers.tf
+++ providers.tf
@@ -1,4 +1,5 @@
 provider "yandex" {
+  token                    = yc_token
   cloud_id                 = local.cloud_id
   folder_id                = local.folder_id
   zone                     = local.yc_zone
