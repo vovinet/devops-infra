84bdd6a2cc030996dec57b70e137b8a84a3030e3
Vladimir Zubarev <v.zubarev@inbox.ru>
2022-07-09 15:33:08 +0300

    auth changed

--- stage/variables.tf
+++ stage/variables.tf
@@ -3,3 +3,4 @@ variable "folder_id" {}
 variable "yc_zone" {}
 variable "os_image_id" {}
 variable "ssh_pub_key" {}
+variable "service_account_key_file" {}
--- stage/versions.tf
+++ stage/versions.tf
@@ -20,4 +20,5 @@ provider "yandex" {
     cloud_id = var.cloud_id
     folder_id = var.folder_id
     zone = var.yc_zone
+    service_account_key_file = var.service_account_key_file
 }
