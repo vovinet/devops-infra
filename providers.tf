0e7f3b5138b137c6d5641cd72dfc2fd06b5cface
Vladimir Zubarev <v.zubarev@inbox.ru>
2022-07-09 12:23:26 +0300

    YC Prodiver added

--- providers.tf
+++ providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
 provider "yandex" {
   cloud_id                 = local.cloud_id
   folder_id                = local.folder_id
