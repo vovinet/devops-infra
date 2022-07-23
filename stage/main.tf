58bb30c65530cf390de2f3e57c8e71e2aa21314d
Vladimir Zubarev <v.zubarev@inbox.ru>
2022-07-23 19:27:31 +0300

    YC has awesome docs

--- stage/main.tf
+++ stage/main.tf
@@ -44,7 +44,7 @@ resource "yandex_compute_instance" "stage-k8s-cp1" {
   network_interface {
     subnet_id = yandex_vpc_subnet.stage-subnet.id
     ip_address = "10.0.0.10"
-    nat_ip_address = yandex_vpc_address.addr_cp.address
+    nat_ip_address = yandex_vpc_address.addr_cp
     nat       = true
   }
 
@@ -78,7 +78,7 @@ resource "yandex_compute_instance" "stage-k8s-node1" {
   network_interface {
     subnet_id = yandex_vpc_subnet.stage-subnet.id
     ip_address = "10.0.0.11"
-    nat_ip_address = yandex_vpc_address.addr_node.address
+    nat_ip_address = yandex_vpc_address.addr_node
     nat       = true
   }
 
