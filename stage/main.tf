42b68523bd611b7efd9155c4ad2744a54e5c976c
Vladimir Zubarev <v.zubarev@inbox.ru>
2022-07-23 18:26:21 +0300

    static ip added

--- stage/main.tf
+++ stage/main.tf
@@ -6,7 +6,7 @@ resource "yandex_vpc_subnet" "stage-subnet" {
   name           = "Stage-Subnet-0"
   zone           = var.yc_zone
   network_id     = "${yandex_vpc_network.stage_net.id}"
-  v4_cidr_blocks = ["10.0.0.0/21"]
+  v4_cidr_blocks = ["10.0.0.0/24"]
 }
 
 resource "yandex_compute_instance" "stage-k8s-cp1" {
@@ -29,6 +29,7 @@ resource "yandex_compute_instance" "stage-k8s-cp1" {
 
   network_interface {
     subnet_id = yandex_vpc_subnet.stage-subnet.id
+    ip_address = "10.0.0.11/24"
     nat       = true
   }
 
@@ -61,70 +62,7 @@ resource "yandex_compute_instance" "stage-k8s-node1" {
 
   network_interface {
     subnet_id = yandex_vpc_subnet.stage-subnet.id
-    nat       = true
-  }
-
-  metadata = {
-    user-data = file("cloud_config.yaml")
-  }
-
-  scheduling_policy {
-    preemptible = true
-  }
-}
-
-resource "yandex_compute_instance" "stage-k8s-node2" {
-  name = "stage-k8s-node2"
-  folder_id = var.folder_id
-
-  resources {
-    cores  = 2
-    core_fraction = 100
-    memory = 2
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = var.os_image_id
-      size = 20
-      type = "network-hdd"
-    }
-  }
-
-  network_interface {
-    subnet_id = yandex_vpc_subnet.stage-subnet.id
-    nat       = true
-  }
-
-  metadata = {
-    user-data = file("cloud_config.yaml")
-  }
-
-  scheduling_policy {
-    preemptible = true
-  }
-}
-
-resource "yandex_compute_instance" "stage-node3" {
-  name = "stage-k8s-node3"
-  folder_id = var.folder_id
-
-  resources {
-    cores  = 2
-    core_fraction = 100
-    memory = 2
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = var.os_image_id
-      size = 20
-      type = "network-hdd"
-    }
-  }
-
-  network_interface {
-    subnet_id = yandex_vpc_subnet.stage-subnet.id
+    ip_address = "10.0.0.11/24"
     nat       = true
   }
 
