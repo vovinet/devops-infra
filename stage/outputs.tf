8c7d4a4dca509cb6f2ef5ca345c77558d65f0ecd
Vladimir Zubarev <v.zubarev@inbox.ru>
2022-07-24 14:49:44 +0300

    output improved

--- stage/outputs.tf
+++ stage/outputs.tf
@@ -1,7 +1,13 @@
-output "master_node_ip_address" {
+output "stage_k8s_cp1_private_ip" {
   value = yandex_compute_instance.stage-k8s-cp1[*].network_interface.0.ip_address
 }
+output "stage_k8s_cp1_global_ip" {
+  value = yandex_compute_instance.stage-k8s-cp1[*].network_interface.0.nat_ip_address
+}
 
-output "worker_node_ip_address" {
+output "stage_k8s_node1_private_ip" {
   value = yandex_compute_instance.stage-k8s-node1[*].network_interface.0.ip_address
+}
+output "stage_k8s_node1_global_ip" {
+  value = yandex_compute_instance.stage-k8s-node1[*].network_interface.0.nat_ip_address
 }
