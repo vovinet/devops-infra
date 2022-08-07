31fbc862bfba34becdec2547a89ff298cd9bca65
Vladimir Zubarev <v.zubarev@inbox.ru>
2022-08-07 20:45:36 +0300

    typo fixed

--- stage/main.tf
+++ stage/main.tf
@@ -161,17 +161,17 @@ resource "yandex_alb_target_group" "k8s_tg" {
 
   target {
     subnet_id = "${yandex_vpc_subnet.stage-subnet.id}"
-    ip_address   = "${yandex_compute_instance.stage-k8s-node1[*].network_interface.0.ip_address}"
+    ip_address   = "${yandex_compute_instance.stage-k8s-node1.network_interface.0.ip_address}"
   }
 
   target {
     subnet_id = "${yandex_vpc_subnet.stage-subnet.id}"
-    ip_address   = "${yandex_compute_instance.stage-k8s-node2[*].network_interface.0.ip_address}"
+    ip_address   = "${yandex_compute_instance.stage-k8s-node2.network_interface.0.ip_address}"
   }
 
   target {
     subnet_id = "${yandex_vpc_subnet.stage-subnet.id}"
-    ip_address   = "${yandex_compute_instance.stage-k8s-node3[*].network_interface.0.ip_address}"
+    ip_address   = "${yandex_compute_instance.stage-k8s-node3.network_interface.0.ip_address}"
   }
 }
 
