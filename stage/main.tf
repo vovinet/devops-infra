87aa17f45acf59d647af431576fdc6d21c8f2474
Vladimir Zubarev <v.zubarev@inbox.ru>
2022-08-07 20:31:51 +0300

    ALB introduced

--- stage/main.tf
+++ stage/main.tf
@@ -1,11 +1,11 @@
-resource "yandex_vpc_network" "stage_net" {
+resource "yandex_vpc_network" "stage-net" {
   name = "Stage-Net"
 }
 
 resource "yandex_vpc_subnet" "stage-subnet" {
   name           = "Stage-Subnet-0"
   zone           = var.yc_zone
-  network_id     = "${yandex_vpc_network.stage_net.id}"
+  network_id     = "${yandex_vpc_network.stage-net.id}"
   v4_cidr_blocks = ["10.0.0.0/24"]
 }
 
@@ -153,5 +153,135 @@ resource "yandex_compute_instance" "stage-k8s-node3" {
 
   scheduling_policy {
     preemptible = true
+  }
+}
+
+resource "yandex_alb_target_group" "k8s_tg" {
+  name      = "K8s-target-group"
+
+  target {
+    subnet_id = "${yandex_vpc_subnet.stage-subnet.id}"
+    ip_address   = "${yandex_compute_instance.stage-k8s-node1.network_interface.0.ip_address}"
+  }
+
+  target {
+    subnet_id = "${yandex_vpc_subnet.mstage-subnet.id}"
+    ip_address   = "${yandex_compute_instance.stage-k8s-node2.network_interface.0.ip_address}"
+  }
+
+  target {
+    subnet_id = "${yandex_vpc_subnet.mstage-subnet.id}"
+    ip_address   = "${yandex_compute_instance.stage-k8s-node3.network_interface.0.ip_address}"
+  }
+}
+
+resource "yandex_alb_backend_group" "myapp-bg" {
+  name      = "my-backend-group"
+
+  http_backend {
+    name = "test-http-backend"
+    weight = 1
+    port = 8080
+    target_group_ids = ["${yandex_alb_target_group.k8s-tg.id}"]
+
+    load_balancing_config {
+      panic_threshold = 50
+    }    
+    healthcheck {
+      timeout = "1s"
+      interval = "1s"
+      http_healthcheck {
+        path  = "/"
+      }
+    }
+    http2 = "true"
+  }
+}
+
+resource "yandex_alb_backend_group" "graphics-bg" {
+  name      = "my-backend-group"
+
+  http_backend {
+    name = "test-http-backend"
+    weight = 1
+    port = 3000
+    target_group_ids = ["${yandex_alb_target_group.k8s-tg.id}"]
+
+    load_balancing_config {
+      panic_threshold = 50
+    }    
+    healthcheck {
+      timeout = "1s"
+      interval = "1s"
+      http_healthcheck {
+        path  = "/"
+      }
+    }
+    http2 = "true"
+  }
+}
+
+resource "yandex_alb_http_router" "stage-router" {
+  name      = "stage-http-router"
+  labels = {
+    tf-label    = "tf-label-value"
+    empty-label = ""
+  }
+}
+
+resource "yandex_alb_load_balancer" "stage-balancer" {
+  name        = "stage-load-balancer"
+
+  network_id  = yandex_vpc_network.stage-net.id
+
+  allocation_policy {
+    location {
+      zone_id   = "ru-central1-a"
+      subnet_id = yandex_vpc_subnet.stage-subnet.id 
+    }
+  }
+
+  listener {
+    name = "stage-listener"
+    endpoint {
+      address {
+        external_ipv4_address {
+        }
+      }
+      ports = [ 80 ]
+    }    
+    http {
+      handler {
+        http_router_id = yandex_alb_http_router.stage-router.id
+      }
+    }
+  }    
+}
+
+resource "yandex_alb_virtual_host" "vhost-graphics" {
+  name      = "vhost-graphics"
+  http_router_id = yandex_alb_http_router.stage-router.id
+  route {
+    name = "graphics"
+    http_route {
+      http_route_action {
+        backend_group_id = yandex_alb_backend_group.graphics-bg.id
+        timeout = "3s"
+      }
+    }
+  }
+}
+
+resource "yandex_alb_virtual_host" "vhost-myapp" {
+  name      = "vhost-myapp"
+  http_router_id = yandex_alb_http_router.stage-router.id
+  route {
+    name = "myapp"
+    http_route {
+      http_route_action {
+        backend_group_id = yandex_alb_backend_group.myapp-bg.id
+        timeout = "3s"
+      }
+    }
   }
 }
