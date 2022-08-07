7b63a0bfe7ec8da23920bc6651686bd7193868b9
Vladimir Zubarev <v.zubarev@inbox.ru>
2022-08-07 20:33:45 +0300

    typo fixed

--- stage/main.tf
+++ stage/main.tf
@@ -165,12 +165,12 @@ resource "yandex_alb_target_group" "k8s_tg" {
   }
 
   target {
-    subnet_id = "${yandex_vpc_subnet.mstage-subnet.id}"
+    subnet_id = "${yandex_vpc_subnet.stage-subnet.id}"
     ip_address   = "${yandex_compute_instance.stage-k8s-node2.network_interface.0.ip_address}"
   }
 
   target {
-    subnet_id = "${yandex_vpc_subnet.mstage-subnet.id}"
+    subnet_id = "${yandex_vpc_subnet.stage-subnet.id}"
     ip_address   = "${yandex_compute_instance.stage-k8s-node3.network_interface.0.ip_address}"
   }
 }
